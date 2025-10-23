c0246c95df5ed22ef82a197e1730775903530294
mauraaninditakirana <kiranamaura351@gmail.com>
2025-10-23 11:35:43 +0700

    Menambahkan Text menampilkan nama program studi dan universitas. Menyesuaikan layout Column agar memenuhi layar dan konten berada di tengah secara horizontal. Menambahkan Spacer untuk memberikan jarak vertikal.

--- app/src/main/java/com/example/per4/UIPertama.kt
+++ app/src/main/java/com/example/per4/UIPertama.kt
@@ -5,9 +5,18 @@ import androidx.compose.runtime.Composable
 @Composable
 fun ActivitasPertama(modifier: Modifier) {
     Column(modifier = Modifier.padding(top=100.dp)
-    )
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = stringResource(id= R.string.prodi),
+            fontSize = .35sp,
+            fontWeight = FontWeight.Bold)
+        Text(text = stringResource(id= R.string.Univ),
+            fontSize = 22.sp)
+        Spacer(modifier = Modifier.height(height = 25.dp))
+    }
 }
-)
+
 
 
 
