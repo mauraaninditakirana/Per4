5619a056759fcaba027431e00cfbc5da847e03d2
mauraaninditakirana <kiranamaura351@gmail.com>
2025-10-23 11:25:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/per4/UIPertama.kt
@@ -0,0 +1,29 @@
+package com.example.per4
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ActivitasPertama(modifier: Modifier) {
+    Column(modifier = Modifier.padding(top=100.dp)
+    )
+}
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
