714805abdcee44c71fd6da4310b7855b5e8bcbab
mauraaninditakirana <kiranamaura351@gmail.com>
2025-10-23 12:04:17 +0700

    Menerapkan Column untuk Text nama. Menambahkan Text untuk menampilkan nama. Mengatur font, warna, dan padding pada Text.

--- app/src/main/java/com/example/per4/UIPertama.kt
+++ app/src/main/java/com/example/per4/UIPertama.kt
@@ -16,8 +16,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,7 +50,15 @@ fun ActivitasPertama(modifier : Modifier) {
                     modifier = Modifier.size(100.dp).padding(5.dp)
                 )
                 Spacer(modifier = Modifier.width(30.dp))
-
+                Column (){
+                    Text(
+                        stringResource(R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top =15.dp)
+                    )
+                }
             }
         }
     }
