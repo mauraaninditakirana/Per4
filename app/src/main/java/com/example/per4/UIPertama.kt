914ce94d89d3ef4d751791974824d680843c1143
mauraaninditakirana <kiranamaura351@gmail.com>
2025-10-23 11:54:49 +0700

    Menambahkan Image untuk menampilkan logo. Mengatur ukuran dan padding pada Image.

--- app/src/main/java/com/example/per4/UIPertama.kt
+++ app/src/main/java/com/example/per4/UIPertama.kt
@@ -1,5 +1,6 @@
 package com.example.per4
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -39,6 +41,11 @@ fun ActivitasPertama(modifier : Modifier) {
             )){
             Row(){
                 val gambar = painterResource(id = R.drawable.logoumy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(100.dp).padding(5.dp)
+                )
 
             }
         }
